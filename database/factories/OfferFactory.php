cacf6ae30e36bc37911fac2dd6ce1603cf39e069
Aleksey Abramov <dev.abramoff@gmail.com>
2021-02-15 16:46:31 +0300

    added factory for offer model

---
+++ database/factories/OfferFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Offer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OfferFactory extends Factory
+{
+    protected $model = Offer::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => ucwords($this->faker->words(rand(2, 3))),
+            'price' => rand(260000, 600000),
+            'bedrooms' => rand(3, 5),
+            'bathrooms' => rand(2, 3),
+            'storeys' => rand(1, 2),
+            'garages' => rand(1, 3),
+        ];
+    }
+}
