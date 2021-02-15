e89f0bc154fead3866311b851d5c5367a8fd2f7c
Aleksey Abramov <dev.abramoff@gmail.com>
2021-02-15 17:12:58 +0300

    added vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import Vue from 'vue'
+
+import App from './views/app'
+
+const app = new Vue({
+  el: '#app',
+  components: { App },
+})
---
+++ resources/js/views/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  props: {
+    config: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      message: 'Hello World',
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
