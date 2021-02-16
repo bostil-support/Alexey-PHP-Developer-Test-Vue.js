6fd3453adc2461aa74d48a4d78ab56f8cf7d3f4f
Aleksey Abramov <dev.abramoff@gmail.com>
2021-02-16 16:27:21 +0300

    updated ClientController.php

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
 
 class ClientController extends Controller
 {
     public function __invoke(): View
     {
-        return view('welcome', ['filter' => config('filter')]);
+        $filter = config('filter');
+        $filter['min_price'] = DB::table('offers')->min('price');
+        $filter['max_price'] = DB::table('offers')->max('price');
+        return view('welcome', compact('filter'));
     }
 }
