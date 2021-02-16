4dd8767c230aded6c9dd8697edf52fe22f401c2e
Aleksey Abramov <dev.abramoff@gmail.com>
2021-02-16 16:27:51 +0300

    updated OfferController.php

--- app/Http/Controllers/Api/OfferController.php
+++ app/Http/Controllers/Api/OfferController.php
@@ -2,15 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Filters\OfferFilter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OfferListRequest;
 use App\Http\Resources\Api\OfferResource;
+use App\Imports\OffersImport;
 use App\Models\Offer;
+use Excel;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class OfferController extends Controller
 {
-    public function index(): AnonymousResourceCollection
+    public function index(OfferListRequest $request): AnonymousResourceCollection
     {
-        return OfferResource::collection(Offer::all());
+        return OfferResource::collection(
+            Offer::filter(new OfferFilter($request))
+                ->paginate(25)
+        );
+    }
+
+    public function import(Request $request): Response
+    {
+        Excel::import(new OffersImport, $request->file('file'));
+
+        return response()->noContent();
     }
 }
