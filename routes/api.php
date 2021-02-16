cce4f917fdd8814872420767d875751c9006cde7
Aleksey Abramov <dev.abramoff@gmail.com>
2021-02-16 16:28:16 +0300

    added import route

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('offers', \App\Http\Controllers\Api\OfferController::class)->only('index');
+Route::post('offers/import', [\App\Http\Controllers\Api\OfferController::class, 'import']);
